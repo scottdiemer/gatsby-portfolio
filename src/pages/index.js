2b1cb114e46313f6de25330f5fb49bd3600b48c9
scottdiemer <sdiemer@cfl.rr.com>
2020-09-17 20:39:21 -0400

    added particle js to use for hero section. Not sure if I'm going to keep it or not

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import React from "react"
 import Layout from "../components/layout"
 import Projects from "../components/projects"
 import Contact from "../components/contact"
+import Hero from "../components/hero"
 
 const IndexPage = () => {
   return (
     <Layout>
+      <Hero />
       <Projects />
       <Contact />
     </Layout>
