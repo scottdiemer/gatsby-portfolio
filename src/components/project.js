8bddb432ee85dc1d73c358c346702f85d01bc52e
scottdiemer <sdiemer@cfl.rr.com>
2020-09-18 15:13:05 -0400

    adjusted css and elements in project component

--- src/components/project.js
+++ src/components/project.js
@@ -18,43 +18,95 @@ const Project = ({
       <Image fluid={imageData} alt={title} />
     </Link>
     <div className="description">
-      <h2>
-        <Link to={`/${slug}/`}>{title}</Link>
-      </h2>
+      <h5>
+        <Link className="title" to={`/${slug}/`}>
+          {title}
+        </Link>
+      </h5>
       <p>{description}</p>
-      <p>
-        <Link to={`/${slug}/`}>View this project &rarr;</Link>
-      </p>
-      <a href={githubUrl} target="_blank" rel="noopener noreferrer">
-        <FaGithub />
+      {/* <p> */}
+      {/*   <Link to={`/${slug}/`}>View this project &rarr;</Link> */}
+      {/* </p> */}
+      <a
+        href={githubUrl}
+        className="github-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub size="1.5rem" />
       </a>
-      <div className="technology-wrapper">
-        <div className="technology-title">Technologies</div>
-        <div className="technology-badges">
-          {technologies.map(tech => {
-            return <span className="technology">{tech}</span>
-          })}
-        </div>
+    </div>
+    <div className="technology-wrapper">
+      <div className="technology-title">Technologies</div>
+      <div className="technology-badges">
+        {technologies.map(tech => {
+          return <span className="technology">{tech}</span>
+        })}
       </div>
     </div>
   </article>
 )
 
 export default styled(Project)`
+  background-color: var(--secondaryBackgroundColor);
   box-shadow: var(--lightShadow);
-  border-radius: 8px;
   text-align: center;
   margin: 1rem;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
   .description {
-    padding: 1rem;
+    height: 100%;
+    position: relative;
   }
-  img {
-    border-radius: 8px 8px 0 0;
+
+  .github-icon {
+    positon: absolute;
+    bottom: 0;
   }
-  h2 {
-    font-size: 1.5rem;
+
+  .title {
+    text-decoration: none;
+    font-weight: 400;
   }
-  a {
-    color: var(--mainBlack);
+
+  technology-wrapper,
+  .technology-badges {
+    border-radius: 8px;
+  }
+
+  .technology-wrapper {
+    border-radius: 8px;
+  }
+
+  .technology-title {
+    border-top: 1px solid var(--darkGray);
+    border-bottom: 1px solid var(--darkGray);
+    color: var(--offWhite);
+    background-color: hsl(255, 0%, 30%);
+  }
+
+  .technology-badges {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    padding: 0.5rem;
+    background-color: hsl(255, 0%, 22%);
+  }
+
+  .technology-badges .technology {
+    background-color: var(--primaryColor);
+    margin: 4px;
+    padding: 0 0.1rem;
+    border-radius: 3px;
+    font-size: 0.8rem;
+  }
+
+  .github-icon {
+    color: var(--offWhite);
+    height: 150px;
+    width: 150px;
   }
 `
