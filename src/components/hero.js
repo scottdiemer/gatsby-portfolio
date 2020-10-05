cf35b310d636b75a474770b538c1f6161f6a5e18
scottdiemer <sdiemer@cfl.rr.com>
2020-10-05 13:20:12 -0400

    adjusted top margin when flexbox wraps

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
     <section id="hero" className={styles.hero}>
       <div className={styles.callToAction}>
         <h1>Hi, my name is Scott Diemer</h1>
-        <h1>I'm a Developer!</h1>
+        <h1>I'm a Web Developer!</h1>
       </div>
       <div className={styles.heroImageWrapper}>
         <HeroImage />
